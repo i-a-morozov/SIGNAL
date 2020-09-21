1904fd02ef5b85cb547c5d09ad247dcb6ffc38c5
Ivan Morozov <i.a.morozov@inp.nsk.su>
2020-09-21 08:48:12 +0700

    21_09_2020

--- epics/signal.h
+++ epics/signal.h
@@ -12,6 +12,9 @@ double  frequency_(int*, int*, int*, double*, double*, double*) ;
 void    decomposition_(int*, int*, int*, double*, double*, double*, int*, double*, double*, double*) ;
 void    frequency_list_(int*, int*, int*, double*, double*, double*, int*, double*) ;
 void    amplitude_list_(int*, int*, double*, double*, double*, int*, double*, double*, double*) ;
+void    amplitude_(int*, double*, double*, double*, double*, double*, double*, double*) ;
 void    fit_(int*, double*, int*, double*, double*, double*, double*, double*) ;
 void    filter_(int*, double*, int*) ;
+void    compute_table_(int*) ;
+void    destroy_table_(int*) ;
 #endif
